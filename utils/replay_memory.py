5619a0938c41f54a145cdf32c0655cbb91db2d9e
whathelll <whathell@gmail.com>
2018-04-25 16:41:07 +1000

    adding prioritised replay memory

--- utils/replay_memory.py
+++ utils/replay_memory.py
@@ -38,3 +38,53 @@ class ReplayMemory(object):
     for i in range(self.__len__()):
       result.append(self.memory[i].__str__() + " \n")
     return "".join(result)
+
+
+# Proportional: I don't understand SumTrees
+class PrioritisedReplayMemory(object):
+  def __init__(self, capacity, e=0.1, alpha=0.5):
+    self.capacity = capacity
+    self.memory = []
+    self.errors = []
+    self.position = 0
+    self.e = e
+    self.alpha = alpha
+
+  def push(self, item):
+    """Saves a transition."""
+    if len(self.memory) < self.capacity:
+      self.memory.append(None)
+      self.errors.append(None)
+    self.memory[self.position] = item
+    self.errors[self.position] = 10000
+    self.position = (self.position + 1) % self.capacity
+
+  def sample(self, batch_size):
+    probability = np.array(self.errors) ** self.alpha
+    probability = probability / np.sum(probability)
+
+    indices = np.random.choice(len(self.errors), size=batch_size, replace=False, p=probability)
+    out = np.take(self.memory, indices, axis=0)
+    batched = Transition(*zip(*out))
+    s = np.array(list(batched.s))
+    a = np.expand_dims(np.array(list(batched.a)), axis=1)
+    s_1 = np.array(list(batched.s_1))
+    r = np.expand_dims(np.array(list(batched.r)), axis=1)
+    done = np.expand_dims(np.array(list(batched.done)), axis=1)
+    return [s, a, s_1, r, done], indices
+
+  def update(self, indices, errors):
+    errors = np.absolute(errors)
+    for i in range(len(indices)):
+      self.errors[indices[i]] = errors[i] + self.e
+
+  def __len__(self):
+    return len(self.memory)
+
+  def __str__(self):
+    result = []
+    for i in range(self.__len__()):
+      result.append(self.memory[i].__str__() + " error:" + self.errors[i].__str__() + " \n")
+    return "".join(result)
+
+
---
+++ utils/test/test_prioritised_replay_memory.py
@@ -0,0 +1,40 @@
+import unittest
+from utils.replay_memory import PrioritisedReplayMemory, Transition
+
+
+class TestPrioritisedReplayMemory(unittest.TestCase):
+  def setUp(self):
+    self.memory = PrioritisedReplayMemory(capacity=10, e=0.1, alpha=0.5)
+
+  def test_append(self):
+    for i in range(20):
+      a = Transition([0, 1, 2, 3], 0, [4, 5, 6, 7], 0, True)
+      self.memory.push(a)
+    self.assertEqual(len(self.memory.memory), 10)
+
+  # haven't tested the proportional code yet
+  def test_sample(self):
+    for i in range(10):
+      a = Transition([0, 1, 2, i], 0, [4, 5, 6, i*i], 0, True)
+      self.memory.push(a)
+
+    [s, a, s1, r, done], indices = self.memory.sample(2)
+    self.assertEqual(s.shape, (2, 4))
+    self.assertEqual(a.shape, (2, 1))
+    self.assertEqual(s1.shape, (2, 4))
+    self.assertEqual(r.shape, (2, 1))
+    self.assertEqual(done.shape, (2, 1))
+
+  def test_update(self):
+    for i in range(10):
+      a = Transition([0, 1, 2, i], 0, [4, 5, 6, i*i], 0, True)
+      self.memory.push(a)
+
+    self.memory.update([1, 3], [2, 5])
+    self.assertEqual(self.memory.errors[1], 2.1)
+    self.assertEqual(self.memory.errors[3], 5.1)
+
+
+
+if __name__ == "__main__":
+  unittest.main()
